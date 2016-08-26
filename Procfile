2a056d567b4b934f821762016b643d513eee44d4
Eyvind <eyvind.coaquira@gmail.com>
2016-08-26 19:09:58 -0400

    Initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn simuladorTiroEjercitoBackend.wsgi --log-file -
---
+++ soy_digital/apps.py
@@ -0,0 +1,7 @@
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class SoyDigitalConfig(AppConfig):
+    name = 'soy_digital'
