fa8e9bf3010c5e2166f55313525eb0a62dead314
Eyvind <eyvind.coaquira@gmail.com>
2016-08-26 20:18:57 -0400

    Heroku, Django

--- BotMessenger/settings.py
+++ BotMessenger/settings.py
@@ -113,4 +113,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = (
+    os.path.join(os.path.dirname(BASE_DIR), 'static'),
+)
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
